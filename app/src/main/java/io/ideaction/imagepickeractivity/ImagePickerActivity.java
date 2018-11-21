90b4127d317172207cdd57583fcb34adc1e2f8d9
Mihail Gurulea <mihail.gurulea@gmail.com>
2018-11-21 12:56:54 +0200

    Version 21 Nov

---
+++ app/src/main/java/io/ideaction/imagepickeractivity/ImagePickerActivity.java
@@ -0,0 +1,45 @@
+package io.ideaction.imagepickeractivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class ImagePickerActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE = 100;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_picker);
+
+        imageView = findViewById(R.id.image_view);
+
+        Button pickImageButton = findViewById(R.id.pick_image_button);
+        pickImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            Uri imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
+}
